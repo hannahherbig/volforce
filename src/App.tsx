7be6ffa414ac75d29055d6654dea2f9dd742845b
Hannah Herbig <nothannahherbig@gmail.com>
2024-06-19 01:05:42 -0400

    start da changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+// import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button, Container, Navbar } from "react-bootstrap";
+import { Table } from "react-bootstrap";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navbar expand="lg" className="justify-content-between">
+        <Navbar.Brand href="#">Volforce Calculator</Navbar.Brand>
+        <Navbar.Brand href="#">{Math.random().toFixed(4)}</Navbar.Brand>
+      </Navbar>
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th className="text-right">
+              <input type="checkbox" />
+              &nbsp;
+              <span>Edit</span>
+            </th>
+            <th>Name</th>
+            <th className="text-center">Score</th>
+            <th className="text-center">Clear</th>
+            <th className="text-center">Grade</th>
+            <th className="text-center">VF</th>
+            <th className="text-center">
+              <Button variant="info">Sort</Button>
+            </th>
+          </tr>
+        </thead>
+        <tbody></tbody>
+      </Table>
+    </>
   );
 }
 
